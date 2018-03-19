4fd6874a7ed4685c53e89e1f9c6b22692d70559e
xiongben <wangdongkf@163.com>
2018-03-19 10:59:51 +0800

    修复代码

--- src/main/java/com/gws/utils/SqlAccessLog.java
+++ src/main/java/com/gws/utils/SqlAccessLog.java
@@ -1,18 +1,11 @@
-/*
- * Copyright (C) 2016  HangZhou YuShi Technology Co.Ltd  Holdings Ltd. All rights reserved
- *
- * 本代码版权归杭州宇石科技所有，且受到相关的法律保护。
- * 没有经过版权所有者的书面同意，
- * 任何其他个人或组织均不得以任何形式将本文件或本文件的部分代码用于其他商业用途。
- *
- */
+
 package com.gws.utils;
 
 /**
  * sql访问日志
  *
  * @version 
- * @author liuyi  2016年7月18日 上午10:27:26
+ * @author   2016年7月18日 上午10:27:26
  * 
  */
 public class SqlAccessLog {
